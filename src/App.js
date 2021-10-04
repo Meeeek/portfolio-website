495308365689088cf5226934dcd3eaf9c9d52476
Conrad Fukuzawa <conrad.fukuzawa@mail.utoronto.ca>
2021-10-04 16:41:57 -0400

    changed title and desc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="parallax p-1">
+        <div className="main-info-container">
+          
+          <h1 className="my-name">
+            Conrad Fukuzawa
+          </h1>
+
+          <div className="links-container">
+            <h2 className="links-text">
+              links to my stuff
+            </h2>
+            <a href="https://github.com/Meeeek" target="_black">
+              <i className="fa fa-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/conrad-fukuzawa-540468198/" target="_blank">
+              <i className="fa fa-linkedin"></i>
+            </a>
+            <a href="https://www.instagram.com/conradfukuzawa/" target="_blank">
+              <i className="fa fa-instagram"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className="more-info">
+        <div className="info-container">
+          <p className="info-text">
+            Hi my name is Conrad Fukuzawa. I am a computer science student studying at University of Toronto Mississauga.
+
+            <b><br></br>SITE UNDER CONSTRUCTION</b>
+          </p>
+
+          <br></br>
+          <a className="resume-link">
+            <b>
+              LINK TO RESUME
+            </b>
+          </a>
+        </div>
+      </div>
+
+      <div className="parallax p-2">
+
+      </div>
     </div>
   );
 }
